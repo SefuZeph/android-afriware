dfc4200537c3dbb06fe9ee2fd86f78eeeb071bdb
sefu <zephaniahsefuoduori@gmail.com>
2019-12-26 16:42:29 +0300

    Implement ui for the ladies fragment

--- app/src/main/java/com/example/android/android_afriware/ui/ladies/LadiesFragment.kt
+++ app/src/main/java/com/example/android/android_afriware/ui/ladies/LadiesFragment.kt
@@ -2,25 +2,29 @@ package com.example.android.android_afriware.ui.ladies
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.example.android.android_afriware.R
+import androidx.fragment.app.Fragment
+import com.example.android.android_afriware.databinding.FragmentLadiesBinding
 
 /**
  * Kitenge fashion for the ladies will be reflected here
  */
 class LadiesFragment : Fragment() {
 
+    private lateinit var binding: FragmentLadiesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ladies, container, false)
+        binding = FragmentLadiesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
 }
